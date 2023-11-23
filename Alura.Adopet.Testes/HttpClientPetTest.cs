b867585e56f41e6ed5e691525da7c56411cc19a4
Lucas Eduardo Ferreira da Rosa <lucas.eferreira@totvs.com.br>
2023-11-23 16:25:00 -0300

    Create mock to test ListPetsAsync

--- Alura.Adopet.Testes/HttpClientPetTest.cs
+++ Alura.Adopet.Testes/HttpClientPetTest.cs
@@ -1,4 +1,8 @@
+using Alura.Adopet.Console;
 using Alura.Adopet.Console.Services;
+using Moq;
+using Moq.Protected;
+using System.Net;
 
 namespace Alura.Adopet.Testes;
 
@@ -8,7 +12,41 @@ public class HttpClientPetTest
     public async Task ListaPetsDeveRetornarUmaListaNaoVazia()
     {
         // Arrange
-        var httpClientPet = new HttpClientPet();
+        var handlerMock = new Mock<HttpMessageHandler>();
+
+        var response = new HttpResponseMessage
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = new StringContent(@"
+                [
+                    {
+                        ""id"": ""456b24f4-19e2-4423-845d-4a80e8854a41"",
+                        ""nome"": ""Romeu"",
+                        ""tipo"": 0,
+                        ""proprietario"": null
+                    },
+                    {
+                        ""id"": ""456b24f4-19e3-4423-845d-4a80e8854a41"",
+                        ""nome"": ""Lexie"",
+                        ""tipo"": 1,
+                        ""proprietario"": null
+                    }
+                 ]")
+        };
+
+        handlerMock
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>
+                ("SendAsync",
+                  ItExpr.IsAny<HttpRequestMessage>(),
+                  ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(response);
+
+        var httpClient = new Mock<HttpClient>(MockBehavior.Default, handlerMock.Object);
+
+        httpClient.Object.BaseAddress = new Uri("http://localhost:5057");
+
+        var httpClientPet = new HttpClientPet(httpClient.Object);
 
         // Act
         var lista = await httpClientPet.ListPetsAsync();
@@ -22,7 +60,8 @@ public class HttpClientPetTest
     public async Task APIForaDeveRetornarUmaExcecao()
     {
         // Arrange
-        var httpClientPet = new HttpClientPet("http://localhost:1010");
+        var httpClient = new Mock<HttpClient>(MockBehavior.Default);
+        var httpClientPet = new HttpClientPet(httpClient.Object);
 
         // Act + Assert
         // Valida se retorna qualquer exceção do tipo HttpRequestException
